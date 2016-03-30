e85d47605c776b3b44a53cfd91da4c8acb837a59
Alex Agranovich <a.agranovich@dax-service.com>
2016-03-30 16:19:51 +0300

    Initial commit

---
+++ DAXService/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DAXService.Startup))]
+namespace DAXService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
