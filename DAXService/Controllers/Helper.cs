763193bd8bbecdc6eb7a51f8540a3fc3196fe57d
Alexander Agranovich <aag@alfapeople.com>
2016-03-31 23:43:36 +0300

    Language iteration

---
+++ DAXService/Controllers/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DAXService.Controllers
+{
+    public static class Helper
+    {
+        public static string getLanguage(string language)
+        {
+            return language ?? "EN";
+        }
+    }
+}
--- DAXService/Controllers/HomeController.cs
+++ DAXService/Controllers/HomeController.cs
@@ -8,15 +8,20 @@ namespace DAXService.Controllers
 {
     public class HomeController : Controller
     {
+        string language;
+
         public ActionResult Index()
         {
             ViewBag.Title = "DAX Service";
+            ViewBag.Language = language;
+
             return View();
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "About page";
+            ViewBag.Language = language;
 
             return View();
         }
@@ -24,9 +29,36 @@ namespace DAXService.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Contact page";
+            ViewBag.Language = language;
 
             return View();
         }
 
+        public ActionResult ChangeLanguageDE()
+        {
+            language = "DE";
+
+            ViewBag.Language = "DE";
+
+            return View("Index");
+        }
+
+        public ActionResult ChangeLanguageEN()
+        {
+            language = "EN";
+
+            ViewBag.Language = "EN";
+
+            return View("Index");
+        }
+
+        public ActionResult ChangeLanguageRU()
+        {
+            language = "RU";
+
+            ViewBag.Language = "RU";
+
+            return View("Index");
+        }
     }
 }
