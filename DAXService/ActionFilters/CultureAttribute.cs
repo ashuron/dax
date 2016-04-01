e9b56c20f1e23edbb054b006699fbc73b6c52887
Alexander Agranovich <aag@alfapeople.com>
2016-04-01 23:12:29 +0300

    Added lost files

---
+++ DAXService/ActionFilters/CultureAttribute.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DAXService.ActionFilters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class CultureAttribute : ActionFilterAttribute
+    {
+        private const string CookieLangEntry = "language";
+
+        public string Name { get; set; }
+
+        public static string CookieName
+        {
+            get { return "_Language"; }
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var culture = Name;
+            if (string.IsNullOrEmpty(culture))
+            {
+                culture = GetSavedCultureOrDefault(filterContext.RequestContext.HttpContext.Request);
+            }
+            // Set culture on current thread
+            SetCultureOnThread(culture);
+
+            // Proceed as usual
+            base.OnActionExecuting(filterContext);
+        }
+
+        public static void SavePreferredCulture(HttpResponseBase response, string language,
+                                                Int32 expireDays = 1)
+        {
+            var cookie = new HttpCookie(CookieName) { Expires = DateTime.Now.AddDays(expireDays) };
+            cookie.Values[CookieLangEntry] = language;
+            response.Cookies.Add(cookie);
+        }
+
+        public static string GetSavedCultureOrDefault(HttpRequestBase httpRequestBase)
+        {
+            var culture = "EN";
+            var cookie = httpRequestBase.Cookies[CookieName];
+            if (cookie != null)
+            {
+                culture = cookie.Values[CookieLangEntry];
+            }
+
+            return culture;
+        }
+
+        private static void SetCultureOnThread(string language)
+        {
+            var cultureInfo = CultureInfo.CreateSpecificCulture(language);
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+        }
+    }
+}
---
+++ DAXService/js/dax.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    // Hide Language B when the web page loads
+    $('.languageEN').hide();
+    $('.languageDE').click(function () {
+        // find all content with .languageA under the div post-content and hide it
+        $(this).parent().find('.languageDE').fadeToggle('slow', function () {
+            // find all content with .languageB under the div post-content and show it
+            $(this).parent().find('.languageEN').show();
+        });
+    });
+    $('.languageEN').click(function () {
+        // find all content with .languageB under the div post-content and hide it
+        $(this).parent().find('.languageEN').fadeToggle('slow', function () {
+            // find all content with .languageA under the div post-content and show it
+            $(this).parent().find('.languageDE').show();
+        });
+    });
+});
